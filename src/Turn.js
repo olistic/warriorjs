3368a08c61bd60e54538b8130d3ae3ec88c231d0
Leonardo Val <leonardo.val@creatartis.com>
2015-06-02 11:32:55 -0300

    Turn's playerObject
    Rolled back previous changes, and replaced them with a player object
    proxy in fashion similar to Space's `playerObject`.

--- src/Turn.js
+++ src/Turn.js
@@ -1,36 +1,57 @@
-class Turn {
-	constructor(actions, senses) {
-		var turn = this,
-			_action = null,
-			_senses = {};
+﻿class Turn {
+  constructor(actions, senses) {
+    this._action = null;
+    this._senses = {};
 
-		Object.keys(actions).forEach((name) => {
-			Object.defineProperty(this, name, {
-				value: (...args) => {
-					if (_action) {
-						throw new Error('Only one action can be performed per turn.');
-					}
-					_action = [name, args];
-				}
-			});
-		});
+    Object.keys(actions).forEach((name) => {
+      this.addAction(name);
+    });
 
-		Object.keys(senses).forEach((name) => {
-			var sense = senses[name];
-			_senses[name] = sense;
-			Object.defineProperty(this, name, {
-				value: (...args) => {
-					return _senses[name].perform(...args);
-				}
-			});
-		});
-		
-		Object.defineProperty(this, 'getAction', {
-			value: () => {
-				return _action;
-			}
-		});
-	}
+    Object.keys(senses).forEach((name) => {
+      this.addSense(name, senses[name]);
+    });
+  }
+
+  getAction() {
+    return this._action;
+  }
+
+  addAction(name) {
+    Object.defineProperty(this, name, {
+      value: (...args) => {
+        if (this._action) {
+          throw new Error('Only one action can be performed per turn.');
+        }
+
+        this._action = [name, args];
+      },
+      enumerable: true
+    });
+  }
+
+  addSense(name, sense) {
+    this._senses[name] = sense;
+    Object.defineProperty(this, name, {
+      value: (...args) => {
+        return this._senses[name].perform(...args);
+      },
+	  enumerable: true
+    });
+  }
+  
+  /** Makes a new object that acts like a proxy of a Turn. Allows to access its methods without
+  the risk of exposing intimate details of the implementation, to the code provided by the user.
+  */
+  playerObject() {
+    var result = {};
+	var turn = this;
+	Object.keys(this).forEach((id) => {
+	  if (typeof turn[id] === 'function') {
+        result[id] = turn[id].bind(turn);
+	  }
+    });
+    return result;
+  }
 }
 
 export default Turn;
