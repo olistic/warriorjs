6e0c86d3d6758038a3bcfb71eda6cd0e296a0ff9
Matias Olivera <moliverafreire@gmail.com>
2018-05-18 16:45:22 -0300

    Improve geography functions signature (drive-by)

--- packages/warriorjs-geography/src/getAbsoluteOffset.test.js
+++ packages/warriorjs-geography/src/getAbsoluteOffset.test.js
@@ -1,7 +1,7 @@
-import { EAST, NORTH, SOUTH, WEST } from './absoluteDirections';
 import getAbsoluteOffset from './getAbsoluteOffset';
+import { EAST, NORTH, SOUTH, WEST } from './absoluteDirections';
 
-test('returns the absolute offset based on orientation', () => {
+test('returns the absolute offset based on direction', () => {
   expect(getAbsoluteOffset([1, 2], NORTH)).toEqual([2, -1]);
   expect(getAbsoluteOffset([1, 2], EAST)).toEqual([1, 2]);
   expect(getAbsoluteOffset([1, 2], SOUTH)).toEqual([-2, 1]);
